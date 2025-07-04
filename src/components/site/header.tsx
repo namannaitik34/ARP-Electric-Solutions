f0ddf2f07713046dba344a74706e422934d0ba3a
naman_naitik34 <kumarnaitik7970@gmail.com>
2025-07-04 09:01:33 +0000

    increase the height of navbar a liitle beat

--- src/components/site/header.tsx
+++ src/components/site/header.tsx
@@ -74,7 +74,7 @@ export function Header() {
         </div>
       </div>
       <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-        <div className="container flex h-16 items-center">
+        <div className="container flex h-20 items-center">
             <div className="mr-4 flex items-center">
                 <Link href="/" className="flex items-center space-x-2">
                     <Logo />
